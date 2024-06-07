cc10a2e4de3975329956675ce5ab67ad73a9cc1c
Yunus İNAN <yunus.inan@demirorenteknoloji.com>
2024-06-07 21:26:56 +0300

    refactor: Add duplicate image display in uploaded images area

--- src/components/uploadedImagesArea/index.tsx
+++ src/components/uploadedImagesArea/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { UploadAreaProps } from "../../Types/type";
-import { UploadedImagesAreaContainer, ImagesWrapper, HeaderTitle } from "./style";
+import { UploadedImagesAreaContainer, ImagesWrapper, HeaderTitle, DublicateImageWrapper, ImageWrapper, FileName } from "./style";
 
 const UpladedImagesArea = ({ duplicates }: UploadAreaProps) => {
 	return (
@@ -8,24 +8,24 @@ const UpladedImagesArea = ({ duplicates }: UploadAreaProps) => {
 			<HeaderTitle>Duplicate Images</HeaderTitle>
 			<ImagesWrapper>
 				{duplicates.map((duplicate, index) => (
-					<div key={index} style={{ marginBottom: "20px", display: "flex" }}>
-						<div>
-							<h3 style={{ marginRight: "20px" }}> {duplicate.fileName}</h3>
+					<DublicateImageWrapper key={index} style={{ marginBottom: "20px", display: "flex" }}>
+						<ImageWrapper>
 							<img
 								src={duplicate.source}
 								alt={duplicate.fileName}
 								style={{ width: "100px", height: "100px", marginRight: "20px" }}
 							/>
-						</div>
-						<div>
-							<h3>{duplicate.fileName2}</h3>
+							<FileName style={{ marginRight: "20px" }}> {duplicate.fileName}</FileName>
+						</ImageWrapper>
+						<ImageWrapper>
 							<img
 								src={duplicate.source2}
 								alt={duplicate.fileName2}
 								style={{ width: "100px", height: "100px" }}
 							/>
-						</div>
-					</div>
+							<FileName>{duplicate.fileName2}</FileName>
+						</ImageWrapper>
+					</DublicateImageWrapper>
 				))}
 			</ImagesWrapper>
 		</UploadedImagesAreaContainer>
--- src/components/uploadedImagesArea/style.ts
+++ src/components/uploadedImagesArea/style.ts
@@ -12,4 +12,30 @@ export const HeaderTitle = styled.h2`
 	margin-bottom: 20px;
 `;
 
-export const ImagesWrapper = styled.div``;
+export const ImagesWrapper = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const DublicateImageWrapper = styled.div`
+	margin-bottom: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const ImageWrapper = styled.div`
+	width: 300px;
+	height: 150px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+`;
+
+export const FileName = styled.p`
+	margin-top: 10px;
+`;
