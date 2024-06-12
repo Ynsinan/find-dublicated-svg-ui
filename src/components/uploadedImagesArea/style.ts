b0a8e5ef311724562b31aa72ec180a7021859b54
Yunus İNAN <yunus.inan@demirorenteknoloji.com>
2024-06-12 23:51:33 +0300

    refactor: Improve styling of duplicate image wrapper in uploaded images area

--- src/components/uploadedImagesArea/style.ts
+++ src/components/uploadedImagesArea/style.ts
@@ -15,18 +15,27 @@ export const HeaderTitle = styled.h2`
 export const ImagesWrapper = styled.div`
 	width: 100%;
 	display: flex;
-	flex-direction: column;
+	/* flex-direction: column; */
 	align-items: center;
 	justify-content: center;
+	flex-wrap: wrap;
+	gap: 10px;
 `;
 
 export const DublicateImageWrapper = styled.div`
-	margin-bottom: 20px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px; // Dolgu ekleyin
+    border: 1px solid #706fd3; // Sınır ekleyin
+    border-radius: 5px; // Köşeleri yuvarlayın
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12); // Bir gölge ekleyin
 
+    &:hover {
+        box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3); // Hover efekti için bir gölge ekleyin
+    }
+`;
 export const ImageWrapper = styled.div`
 	width: 300px;
 	height: 150px;
